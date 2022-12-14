48c7c19008d836f8e5b8115b44e1decd8ee62f41
mariadhantia <mariadhantia@gmail.com>
2022-12-15 03:05:59 +0500

    appJS theme

--- web/src/App.js
+++ web/src/App.js
@@ -17,11 +17,11 @@ function App() {
   const theme = {
     colors: {
       heading: "rgb(24 24 29)",
-      text: "rgb(24 24 29)",
-      white: "fff",
+      text: "rgba(29 29 29 .8)",
+      white: "#fff",
       black: "#212529",
       helper: "#8490ff",
-      bg: "rgb(249 249 255)",
+      bg: "#F6F8FA",
       footer_bg: "#0a1435",
       btn: "rgb(98 84 243)",
       border: "rgba(98 84 243 0.5)",
