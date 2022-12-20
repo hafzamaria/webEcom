712e2686ea8c4c8488458927e1a220c754e55782
mariadhantia <mariadhantia@gmail.com>
2022-12-21 02:21:04 +0500

    'fetch-data'

--- web/src/context/productcontext.mjs
+++ web/src/context/productcontext.mjs
@@ -1,11 +1,25 @@
 // creat context
 // provider
 // consumer => useContext Hook
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 const AppContext = createContext();
-
+// const API = 'http://localhost:5000/Products'
+const API = "https://api.pujakaitem.com/api/products";
 
 const AppProvider =({children}) =>{
+
+const getProducts=async (url)=>{
+const res = await axios.get(url);
+const products = res.data;
+console.log(products)
+console.log(res)
+}
+
+    useEffect(()=>{
+      getProducts(API);
+    },[])
+
     return<AppContext.Provider value={{myName:'Maria'}}>{children}</AppContext.Provider>
 };
 
