a40af3d36669f53ecfb1b060b4181d07acf0a753
mariadhantia <mariadhantia@gmail.com>
2022-12-29 02:16:09 +0500

    product-structure

--- web/src/Products.jsx
+++ web/src/Products.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-
+import FilterSection from './components/FilterSection';
+import Sort from './components/Sort';
+import ProductList from './components/ProductList';
+import styled from "styled-components";
 const Products = () => {
   return (
-    <div>
-      I am products
-    </div>
+    <Wrapper>
+      <div className="container grid grid-filter-column">
+        <div className='filter-section'>
+          <FilterSection/>
+        </div>
+         <section className='product-view--sort'>
+          <div className="sort-filter">
+            <Sort/>
+          </div>
+          <div className="main-product">
+            <ProductList/>
+          </div>
+         </section>
+      </div>
+
+    </Wrapper>
   )
+};
+const Wrapper = styled.section`
+.container{
+  max-width:60vw;
 }
+.filter-section{
+  width:20%;
+}
+.product-view--sort{
+  width:80%;
+}
+  .grid-filter-column {
+    grid-template-columns: 0.2fr 1fr;
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid-filter-column {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
+
 
 export default Products
