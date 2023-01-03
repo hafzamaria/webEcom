d6e59ddc3fa805141bbdef09531af17f198286da
mariadhantia <mariadhantia@gmail.com>
2023-01-04 01:47:06 +0500

    sorting

--- web/src/components/Sort.jsx
+++ web/src/components/Sort.jsx
@@ -5,7 +5,7 @@ import {BsFillGridFill ,BsList } from "react-icons/bs";
 import {useFilterContext} from "../context/filtercontext";
 
 const Sort = () => {
-  const {grid_view ,setGridView,setListView,filter_products} = useFilterContext();
+  const {grid_view ,setGridView,setListView,filter_products,sorting} = useFilterContext();
   return(
     <Wrapper className='sort-section'>
       {/* 1stColumn */}
@@ -21,7 +21,19 @@ const Sort = () => {
       <div className="products-data"><p>{`${filter_products.length} Products Available`}</p></div>
       {/* 3rdColumn */}
       <div className="sort-selection">
-        dropDown
+    <form action="#">
+      <label htmlFor='sort'></label>
+      <select name='sort' id='sort' className='sort-selection--style' onClick={sorting}> 
+        <option value="lowest">Price(lowest)</option>
+       <option value="#" disabled></option>
+        <option value="highest">Price(highest)</option>
+        <option value="#" disabled></option>
+        <option value="a-z">Price(a-z)</option>
+        <option value="#" disabled></option>
+        <option value="z-a" >Price(z-a)</option>
+        
+      </select>
+    </form>
       </div>
 
     </Wrapper>
--- web/src/context/filtercontext.jsx
+++ web/src/context/filtercontext.jsx
@@ -9,8 +9,9 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view:false,
-  // grid_view:true,
+  // grid_view:false,
+  grid_view:true,
+  sorting_value:"lowest",
 
 
 };
@@ -31,12 +32,24 @@ const setListView = ()=>{
   return dispatch ({type:"SET_LIST_VIEW"});
   };
 
+  // sorting function
+  const sorting = ( )=>{
+  dispatch({type:"GET_SORT_VALUE"});
+  }
+
+
+  // to sort the products
+  useEffect(()=>{
+  //  console.log("hiii")
+  dispatch({type:"SORTING_PRODUCTS" , payload:products});
+  },[state.sorting_value]);
+
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state ,setGridView,setListView}}>
+    <FilterContext.Provider value={{ ...state ,setGridView,setListView,sorting}}>
       {children}
     </FilterContext.Provider>
   );
