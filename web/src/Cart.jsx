6941f2286ec13293efb11e7e5ddd39a483e10be8
mariadhantia <mariadhantia@gmail.com>
2023-01-13 01:58:17 +0500

    NoItemInCart

--- web/src/Cart.jsx
+++ web/src/Cart.jsx
@@ -6,8 +6,18 @@ import { NavLink } from "react-router-dom";
 
 const Cart = () => {
   const {cart ,clearCart} =useCartContext();
-  console.log("cart:" , cart);
-  return <Wrapper>
+  // console.log("cart:" , cart);
+
+ if(cart.length === 0){
+  return(
+    <EmptyDiv>
+      <h3>No Items in Cart </h3>
+    </EmptyDiv>
+  )
+ }
+
+  return (
+  <Wrapper>
    <div className="container">
     <div className="cart-heading grid grid-five-column">
       <p>Item</p>
@@ -35,8 +45,19 @@ const Cart = () => {
       
     </div>
    </div>
-  </Wrapper>;
+  </Wrapper>
+  )
 };
+const EmptyDiv = styled.div`
+  display: grid;
+  place-items: center;
+  height: 50vh;
+  h3 {
+    font-size: 4.2rem;
+    text-transform: capitalize;
+    font-weight: 300;
+  }
+`;
 
 const Wrapper = styled.section`
   padding: 9rem 0;
